3b5ae76f681249bc863da7bf6522b092d1e01de2
fatima0205 <126772022+fatima0205@users.noreply.github.com>
2025-10-27 19:14:20 +0100

    Update GameTypes.hs
    actualizacion gametypes a corde al renderizado y al main

--- Modulos/GameTypes.hs
+++ Modulos/GameTypes.hs
@@ -5,24 +5,32 @@ import Graphics.Gloss.Interface.IO.Game (Event(..), Key(..), SpecialKey(..), Key
 
 data Modo = Menu | Jugando deriving (Eq, Show)
 
+data ExplosionType = ImpactExplosion | DeathExplosion
+  deriving (Show, Eq)
+
+data Explosion = Explosion
+  { explosionPos  :: (Float, Float)
+  , explosionTime :: Float
+  , explosionType :: ExplosionType
+  } deriving (Show)
+
 data GameState = GameState
-  { mundo  :: Mundo
-  , tiempo :: Float
-  , ronda  :: Int
-  , modo   :: Modo
+  { mundo      :: Mundo
+  , tiempo     :: Float
+  , ronda      :: Int
+  , modo       :: Modo
+  , explosions :: [Explosion]
   } deriving (Show)
 
 -- Event handler básico para el menú
 handleEvent :: Event -> GameState -> IO GameState
-handleEvent (EventKey (SpecialKey KeySpace) Down _ _) gs =
-  if modo gs == Menu then pure gs { modo = Jugando } else pure gs
-handleEvent (EventKey (Char ' ') Down _ _) gs =
+handleEvent (EventKey (SpecialKey KeyEnter) Down _ _) gs =
   if modo gs == Menu then pure gs { modo = Jugando } else pure gs
 
 -- Reiniciar (R) cuando estás jugando: vuelve al menú con mundo nuevo
 handleEvent (EventKey (Char 'r') Down _ _) gs =
   if modo gs == Jugando
-    then pure gs { modo = Menu }  -- el mundo se vuelve a crear en Main si quisieras; por ahora mantenemos
+    then pure gs { modo = Menu }
     else pure gs
 
 -- Ignorar todo lo demás
